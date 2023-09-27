3e3c0d67216bcc9019ea8d2727868156b20939d6
yaroslawv-nexus <124203359+yaroslawv-nexus@users.noreply.github.com>
2023-09-27 12:47:56 +0300

    f

--- src/index.js
+++ src/index.js
@@ -8,6 +8,5 @@ import { store } from "./redux/store";
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}><App /></Provider>
-    
   </React.StrictMode>
 );
